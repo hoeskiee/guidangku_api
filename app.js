334ad71967668a5d7cf15b18ecc7629ba8099e1e
hoesk1ee <hoeskiee@gmail.com>
2021-12-12 12:05:55 +0700

    asd

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const host = '0.0.0.0';
 
 const app = express();
@@ -18,5 +18,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log(`App is listening on port http://localhost:${port}`)
+    console.log(`App is listening on port http://localhost:${PORT}}`)
 })
