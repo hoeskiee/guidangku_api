12b4baafb99b73bb742da34e0fea68e0d10c3617
hoesk1ee <hoeskiee@gmail.com>
2021-12-12 11:51:15 +0700

    test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 let port = process.env.PORT || 3000;
+const host = '0.0.0.0';
 
 const app = express();
 
@@ -12,6 +13,10 @@ app.use((err, req, res, next) => {
     res.json(err);
 });
 
-app.listen(port, () => {
+app.get('/', (req, res) => {
+    res.send("hello world");
+})
+
+app.listen(port, host, () => {
     console.log(`App is listening on port http://localhost:${port}`)
 })
