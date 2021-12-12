9516a0e54d637015a3b2e80674e4388e3996cfbc
hoesk1ee <hoeskiee@gmail.com>
2021-12-12 12:08:58 +0700

    adding config

---
+++ secrets/dbconfiguration.js
@@ -0,0 +1,7 @@
+module.exports = {
+    user: 'ytgfhdvzrgwzag',
+    host: 'ec2-3-228-75-39.compute-1.amazonaws.com',
+    database: 'da9upeg3vpsg2',
+    port: 5432,
+    password: 'fa001bf3ef98593f6034dfd96321b106ee9f68dd9fe83a815c16a4d52106816b'
+}
