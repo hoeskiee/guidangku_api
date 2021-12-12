6af77831842f394ca6ce9c3243d864928e8edd65
hoesk1ee <hoeskiee@gmail.com>
2021-12-12 12:22:48 +0700

    asd

--- db/index.js
+++ db/index.js
@@ -1,7 +1,18 @@
 const { user, host, database, port, password } = require('../secrets/dbconfiguration');
 const { Pool } = require('pg');
+const { use } = require('../routes');
 
-const pool = new Pool({ user, host, database, port, password});
+const env = process.env.NODE_ENV || 'development';
+
+let connectionString = {
+    user: 'ytgfhdvzrgwzag',
+    host: 'ec2-3-228-75-39.compute-1.amazonaws.com',
+    database: 'da9upeg3vpsg2',
+    port: 5432,
+    password: 'fa001bf3ef98593f6034dfd96321b106ee9f68dd9fe83a815c16a4d52106816b'
+}
+
+const pool = new Pool(connectionString);
 
 module.exports = pool;
 
