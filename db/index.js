8a877a9cd410ff9117b2488b1e9c23285f1e7135
hoesk1ee <hoeskiee@gmail.com>
2021-12-21 00:29:18 +0700

    asd

--- db/index.js
+++ db/index.js
@@ -1,33 +1,19 @@
 // const { user, host, database, port, password } = require('../secrets/dbconfiguration');
-require('dotenv').config();
 const { Pool } = require('pg');
-
-const isProduction = process.env.NODE_ENV === "production";
 
 // const env = process.env.NODE_ENV || 'development';
 
-// let connectionString = {
-//     user: 'ytgfhdvzrgwzag',
-//     host: 'ec2-3-228-75-39.compute-1.amazonaws.com',
-//     database: 'da9upeg3vpsg2',
-//     port: 5432,
-//     password: 'fa001bf3ef98593f6034dfd96321b106ee9f68dd9fe83a815c16a4d52106816b',
-// }
+let connectionString = {
+    user: 'ytgfhdvzrgwzag',
+    host: 'ec2-3-228-75-39.compute-1.amazonaws.com',
+    database: 'da9upeg3vpsg2',
+    port: 5432,
+    password: 'fa001bf3ef98593f6034dfd96321b106ee9f68dd9fe83a815c16a4d52106816b'
+}
 
-const connectionString = 
-    'postgres://ytgfhdvzrgwzag:fa001bf3ef98593f6034dfd96321b106ee9f68dd9fe83a815c16a4d52106816b@ec2-3-228-75-39.compute-1.amazonaws.com:5432/da9upeg3vpsg2';
+connectionString = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+}
 
-// connectionString = {
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: process.env.DATABASE_URL ? true : false
-// }
-
-const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    ssl: {
-        rejectUnauthorized: false,
-    }
-});
-
-pool.connect();
-pool.on('connect', () => console.log('connected to db'));
+const pool = new Pool(connectionString);
--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,28 @@
-const { Router } = require('express');
-const pool = require('../db');
+// const { Router } = require('express');
+// const pool = require('../db');
 
-const router = Router();
+// const router = Router();
 
-router.get('/', (request, response, next) => {
-    pool.query('SELECT * FROM users ORDER BY id ASC',
-        (err, res) => {
-            if (err) return next(err);
+// router.get('/', (request, response, next) => {
+//     pool.query('SELECT * FROM users ORDER BY id ASC',
+//         (err, res) => {
+//             if (err) return next(err);
 
-            response.json(res.rows);
-        }
-    );
-});
+//             response.json(res.rows);
+//         }
+//     );
+// });
 
-router.post('/', (request, response, next) => {
-    const { name, phone, email, password } = request.body;
+// router.post('/', (request, response, next) => {
+//     const { name, phone, email, password } = request.body;
 
-    pool.query('INSERT INTO users(name, phone, email, password) VALUES($1, $2, $3, $4'),
-    [ name, phone, email, password ],
-    (err, res) => {
-        if (err) return next(err);
+//     pool.query('INSERT INTO users(name, phone, email, password) VALUES($1, $2, $3, $4'),
+//     [ name, phone, email, password ],
+//     (err, res) => {
+//         if (err) return next(err);
 
-        response.redirect('/users');
-    }
-})
+//         response.redirect('/users');
+//     }
+// })
 
-module.exports = router;
+// module.exports = router;
