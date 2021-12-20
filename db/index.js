bd35b6c0d8618c71b339cf048bdb856df555706e
hoesk1ee <hoeskiee@gmail.com>
2021-12-21 00:33:01 +0700

    asd

--- db/index.js
+++ db/index.js
@@ -3,17 +3,13 @@ const { Pool } = require('pg');
 
 // const env = process.env.NODE_ENV || 'development';
 
-let connectionString = {
-    user: 'ytgfhdvzrgwzag',
-    host: 'ec2-3-228-75-39.compute-1.amazonaws.com',
-    database: 'da9upeg3vpsg2',
-    port: 5432,
-    password: 'fa001bf3ef98593f6034dfd96321b106ee9f68dd9fe83a815c16a4d52106816b'
-}
+// connectionString = {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: { rejectUnauthorized: false },
+// }
 
-connectionString = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-}
-
-const pool = new Pool(connectionString);
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL || 'postgres://ytgfhdvzrgwzag:fa001bf3ef98593f6034dfd96321b106ee9f68dd9fe83a815c16a4d52106816b@ec2-3-228-75-39.compute-1.amazonaws.com:5432/da9upeg3vpsg2',
+    ssl: process.env.DATABASE_URL ? true : false
+});
+pool.connect();
