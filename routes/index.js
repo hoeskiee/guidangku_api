31167d6700290ef9d7a9467fe214d7943d9a622d
hoesk1ee <hoeskiee@gmail.com>
2021-12-21 22:40:15 +0700

    add users router

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const ando = require('./ando');
 const lubrene = require('./lubrene');
+const users = require('./users');
 
 const router = Router();
 
 router.use('/ando', ando);
 router.use('/lubrene', lubrene);
+router.use('/users', users);
 
 module.exports = router;
